1a8024a3b781515a76a248ebacbd9296d91ebee7
Rsirp0c <122833217+Rsirp0c@users.noreply.github.com>
2023-04-09 23:06:34 -0400

    Create Transaction.js

---
+++ PA04/models/Transaction.js
@@ -0,0 +1,15 @@
+'use strict';
+const mongoose = require( 'mongoose' );
+const Schema = mongoose.Schema;
+const ObjectId = mongoose.Schema.Types.ObjectId;
+
+var transactionSchema = Schema( {
+  description: String,
+  amount: Number,
+  category: String,
+  createdAt: Date,
+  userId: {type:ObjectId, ref:'user' }
+});
+
+
+module.exports = mongoose.model( 'transaction', transactionSchema );
